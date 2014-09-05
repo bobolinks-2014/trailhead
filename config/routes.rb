5e663a306e8bfa8a4b213f80748ba00dfb9bd5d0
Sebastian Radloff <sradloff23@gmail.com>
2014-09-04 19:55:40 -0500

    Update routes.rb
    Changed it back to home controller for the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root '/'
-  resources :trails do
-     resources :comments
+  root 'home#index'
+  resources :home do
+    resources :trails do
+      resources :comments
+    end
   end
   resources :sessions
 end
