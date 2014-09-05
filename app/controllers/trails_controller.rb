d0167ce9e4df0c5683e92d21a5c5bbcf5c60c10b
maddisonb & Marek Pyczkowski <pair+maddisonb.marekpwk@devbootcamp.com>
2014-09-05 14:38:48 -0500

    added markers and clustering

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,3 +1,13 @@
 class TrailsController < ApplicationController
+  def index
 
+    if request.xhr?
+      @markers = []
+      respond_to do |format|
+        format.json { render json: @markers.to_json }
+      end
+    else
+      redirect_to '/'
+    end
+  end
 end
