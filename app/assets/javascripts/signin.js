5a3cb1dc3aff6e530a4d47dadbc811929c9df306
& <pair+jrwalker.sradloff23@devbootcamp.com>
2014-09-05 17:39:36 -0500

    added ability to close popup

--- app/assets/javascripts/signin.js
+++ app/assets/javascripts/signin.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   $(".signin-button").on('click', function(event){
     event.preventDefault();
+    $('.signin-form .error').hide();
 
     $('.signin-form').bPopup({
         modalClose: false,
@@ -23,11 +24,26 @@ $(document).ready(function() {
       });
 
       request.done(function(response){
-        var message = response.message
-        $('.signin-form .error').show();
-        $('.signin-form .error').text(message);
+        if (response.success === 0){
+          var message = response.message;
+          $('.signin-form .message').show();
+          $('.signin-form .message').text(message);
+          $('.signin-form .message').css("color","green");
+          $(".signin-form input[name*='email']").val("");
+          $(".signin-form input[name*='password']").val("");
+        }
+        else if (response.success === 1){
+          var message = response.message;
+          $('.signin-form .message').show();
+          $('.signin-form .message').text(message);
+          $('.signin-form .message').css("color","red");
+          $(".signin-form input[name*='password']").val("");
+        }
       });
 
     });
+    });
+  $(".close-button").on('click', function(){
+    $(".signin-form").bPopup().close();
   });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+
       @message = "User Profile Created Successfully"
     else
-      @message = "Not Able to Create a User Profile"  
+      @message = "Not Able to Create a User Profile"
       render '/'
     end
+  end
+
+  private
+
+  def strong_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
