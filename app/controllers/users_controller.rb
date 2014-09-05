c966514d4ae80d691338bc39b1761859bf9f6ccf
Ravi P <ravip0711@gmail.com>
2014-09-05 11:30:58 -0700

    added users controller code for creating a user and created a helper for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new(params[:user])
+    if @user.save
+      @message = "User Profile Created Successfully"
+    else
+      @message = "Not Able to Create a User Profile"  
+      render '/'
+    end
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+
+  def current_user
+    @user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
 end
