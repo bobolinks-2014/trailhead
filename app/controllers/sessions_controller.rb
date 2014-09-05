60217f82f1b0e0365a5283b9fec7af0a0819fbe9
jrwalker <jrobinsonwalker@gmail.com>
2014-09-05 10:10:45 -0500

    sketched user auth method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+	def create
+		user = User.find_by_email(params[:email])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect_to admin_root_path, :notice => "Welcome back, #{user.email}"
+		else
+			flash.now.alert = "Invalid email or password"
+			render "new"
+		end
+	end
 end
