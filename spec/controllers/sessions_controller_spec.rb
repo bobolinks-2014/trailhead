a66bf42de4129dcf2c2abe6cc5b539dc0158d0bb
jrwalker <jrobinsonwalker@gmail.com>
2014-09-06 10:54:27 -0500

    added spec for sessions controller, but the use of .permit in the strong_params method is causing the specs to fail due to NoMethodError: undefined method 'permit' for string. Researched for about 30 mins, no clear path to fixing this error currently so moving on to write other tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
-describe SessionsController do
+
+Rspec.describe SessionsController, :type => :controller do
+
+	before(:each) do 
+		@hiker = User.create!(username: "JohnMuir", email: "hiker@hike.com", password: "testing", password_confirmation: "testing")
+	end
+
+	
+	describe "#create" do
+		it "should create a session and login the user" do
+			expect (session[:user_id]).should be_nil
+			post :create, user: {username: "JohnMuir", :email => "hiker@hike.com", :password => "testing"}.to_json
+			expect(session[:user_id].to eq(@hiker.id))
+		end
+	end
+
+
+	describe "#destroy" do
+		it "should terminate the session and log the user out" do
+			post :create, user: {username: "JohnMuir", :email => "hiker@hike.com", :password => "testing"}.to_json
+			expect(session[:user_id].to eq(@hiker.id))
+			get :destroy
+			expect(session[:user_id]).to be_nil
+		end
+	end
 
 end
+
+
+
