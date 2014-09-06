e6b91d9c8e5c463492dcfb42208a7ad63407c8a4
jrwalker <jrobinsonwalker@gmail.com>
2014-09-06 14:12:39 -0500

    commented out all the shpecks, discarding in favor of full integration testing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-Rspec.describe UsersController, :type => :controller do
-	let(:user) {User.create!(username: "JohnMuir", email: "muir@hiker.com", password: "hiker", password_confirmation: "hiker")}
+# Rspec.describe UsersController, :type => :controller do
+# 	let(:user) {User.create!(username: "JohnMuir", email: "muir@hiker.com", password: "hiker", password_confirmation: "hiker")}
 
-	# describe "#create" do
-	# 	it "creates a new user" do
-	# 		get :create
-	# 		expect(assigns(:user)).to be_a_new(User)
-	# 	end
-	# end
+# 	# describe "#create" do
+# 	# 	it "creates a new user" do
+# 	# 		get :create
+# 	# 		expect(assigns(:user)).to be_a_new(User)
+# 	# 	end
+# 	# end
 	
 
-end
+# end
