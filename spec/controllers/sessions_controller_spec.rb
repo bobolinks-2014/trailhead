b4aad8245bdce95192b17ffe28b1db04cf602bee
jrwalker <jrobinsonwalker@gmail.com>
2014-09-06 11:51:29 -0500

    isolated controller testing discarded in favor of integration testing

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,30 +1,33 @@
 require 'spec_helper'
 
 
-Rspec.describe SessionsController, :type => :controller do
-
-	before(:each) do 
-		@hiker = User.create!(username: "JohnMuir", email: "hiker@hike.com", password: "testing", password_confirmation: "testing")
-	end
+# Rspec.describe SessionsController, :type => :controller do
 
 	
-	describe "#create" do
-		it "should create a session and login the user" do
-			expect (session[:user_id]).should be_nil
-			post :create, user: {username: "JohnMuir", :email => "hiker@hike.com", :password => "testing"}.to_json
-			expect(session[:user_id].to eq(@hiker.id))
-		end
-	end
+
+	
+# 	describe "#create" do
+# 	before(:each) do 
+# 		@hiker = User.create!(username: "JohnMuir", email: "hiker@hike.com", password: "testing", password_confirmation: "testing")
+# 	end
+# 		it "should create a session and login the user" do
+# 			# expect (session[:user_id]).should be_nil
+# 			json = { user: {:email => "hiker@hike.com", :password => "testing"} }
+# 			expect {
+# 				post :create, json.merge(format: :json) 
+# 				}.should change { (session[:user_id] ).should eq(@hiker.id) }
+# 		end
+# 	end
 
 
-	describe "#destroy" do
-		it "should terminate the session and log the user out" do
-			post :create, user: {username: "JohnMuir", :email => "hiker@hike.com", :password => "testing"}.to_json
-			expect(session[:user_id].to eq(@hiker.id))
-			get :destroy
-			expect(session[:user_id]).to be_nil
-		end
-	end
+# 	# describe "#destroy" do
+# 	# 	it "should terminate the session and log the user out" do
+# 	# 		post :create, user: {username: "JohnMuir", :email => "hiker@hike.com", :password => "testing"}.to_json
+# 	# 		expect(session[:user_id].to eq(@hiker.id))
+# 	# 		get :destroy
+# 	# 		expect(session[:user_id]).to be_nil
+# 	# 	end
+# 	# end
 
 end
 
