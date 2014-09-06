e238f74b1024bdc16c70b79dce83b2176804e1dc
jrwalker <jrobinsonwalker@gmail.com>
2014-09-06 11:50:46 -0500

    added session logic to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(strong_params)
     if @user.save
+      session[:user_id] = @user.id
       respond_to do |format|
         format.json {render json: {success: 0, message: "Thanks for signing up!"} }
       end
