312134ca44074c6e3c196978c95ab93a7d70cdea
Sebastian Radloff <sradloff23@gmail.com>
2014-09-07 18:17:48 -0500

    Update infinite.js
    removed add class haha

--- app/assets/javascripts/infinite.js
+++ app/assets/javascripts/infinite.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
       for(i = 0; i < response.length; i++){
         $('.one-comment').last().append(
           '<div class="one-comment" data-id="' + response[i].id + '"><div class="comment-top"><div class="comment-top-left">' + response[i].user.username + ' ' + response[i].date_hiked + '</div><div class="comment-top-right">rating: ' + response[i].rating + ' difficulty: ' + response[i].difficulty + '</div></div><div class="comment-review"><div class="comment-review-body"><p>' + response[i].review + '</p></div></div><div class="comment-bottom"><div class="comment-tip"><p>Tip:' + response[i].tip + ' </p></div><div class="comment-date-hiked">date hiked:' + response[i].date_hiked + '</div></div></div>'
-          ).addClass('haha')
+          )
 
       };
 
