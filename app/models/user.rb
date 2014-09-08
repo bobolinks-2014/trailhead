d4a756b0b2386f5908add4892be654c5fce22bc1
jrwalker <jrobinsonwalker@gmail.com>
2014-09-08 08:46:52 -0500

    added email and password validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
 	has_many :comments
 	has_secure_password
 	validates_presence_of :password, :on => :create
+	validates :password, length: { in: 6..20 }
+	validates_format_of :email, :with => /.+@.+\..+/i, uniqueness: true	
 end
