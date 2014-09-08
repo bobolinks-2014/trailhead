54f885b06f39558d3f84ae477ea2c0973baac467
& <pair+jrwalker.sradloff23@devbootcamp.com>
2014-09-07 21:51:25 -0500

    created a seeds file for comments and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 require 'csv'
 
 CSV.foreach('db/trails.csv', headers: true) do |row|
@@ -11,4 +12,13 @@ CSV.foreach('db/trails.csv', headers: true) do |row|
 	length = row[7]
 
 	Trail.create(name: name, latitude: latitude, longitude: longitude, length: length, city: city, state: state, description: description, length: length, url: url)
+end
+
+
+10.times do
+  User.create(username: Faker::Name.first_name , email: Faker::Internet.email, password: "1234", password_confirmation: "1234")
+end
+
+45.times do
+  Comment.create(user_id: rand(1..10), trail_id: 1, review: Faker::Lorem.paragraph(6), difficulty: rand(1..3), rating: rand(1..5), tip: Faker::Lorem.sentence(1), date_hiked: Time.now.ago(45) )
 end
