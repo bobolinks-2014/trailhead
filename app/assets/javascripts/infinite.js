6ce0e29b4f2b9059fdd28d55bf4126167387cadf
& <pair+jrwalker.sradloff23@devbootcamp.com>
2014-09-07 19:42:17 -0500

    fixed that addition of comments to add at the end of the last comment

--- app/assets/javascripts/infinite.js
+++ app/assets/javascripts/infinite.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 
     request.done(function(response){
       for(i = 0; i < response.length; i++){
-        $('.one-comment').last().append(
+        $('.one-comment').last().after(
           '<div class="one-comment" data-id="' + response[i].id + '"><div class="comment-top"><div class="comment-top-left">' + response[i].user.username + ' ' + response[i].date_hiked + '</div><div class="comment-top-right">rating: ' + response[i].rating + ' difficulty: ' + response[i].difficulty + '</div></div><div class="comment-review"><div class="comment-review-body"><p>' + response[i].review + '</p></div></div><div class="comment-bottom"><div class="comment-tip"><p>Tip:' + response[i].tip + ' </p></div><div class="comment-date-hiked">date hiked:' + response[i].date_hiked + '</div></div></div>'
           )
 
