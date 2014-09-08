95c0755b0ace37a27fe36d1119eb00fa485fe9b8
jrwalker <jrobinsonwalker@gmail.com>
2014-09-08 10:26:13 -0500

    added validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 	has_many :comments
 	has_secure_password
 	validates_presence_of :password, :on => :create
+	validates_presence_of :email, :on => :create
 	validates :password, length: { in: 6..20 }
 	validates_format_of :email, :with => /.+@.+\..+/i, uniqueness: true	
+	validates_uniqueness_of :email, :on => :create
 end
