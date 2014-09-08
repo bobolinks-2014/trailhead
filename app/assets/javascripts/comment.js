eeeb3fddd8c9c9c1f3687c568be4120953ec505e
cjryan28 <cjryan28@gmail.com>
2014-09-08 16:23:02 -0500

    updated comment form so that it's connected to our backend

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,86 +1,77 @@
 $(document).ready(function() {
-
+  var tree = 0;
 
   $(".comment-button").on('click', function(event){
     event.preventDefault();
+    initializeCommentForm();
+    showCommentForm();
 
-    var rangeHash = {0: "easy", 1: "moderate", 2: "hard"}
-    var range = 1.5
-
-    $(".comment-form .fa").css("color", "gray")
-    $(".comment-form input[type=range]").val(range)
-    $(".comment-form #range").text(rangeHash[Math.floor(range)])
-
-    $('.comment-form input[type=range]').change(function(){
-      range = $('.comment-form input[type=range]').val()
+    function initializeCommentForm() {
+      
+      $(".comment-form .fa").css("color", "gray")
+      var rangeHash = {0: "easy", 1: "moderate", 2: "hard"}
+      var range = 1.5
+      $(".comment-form input[type=range]").val(range)
       $(".comment-form #range").text(rangeHash[Math.floor(range)])
-    })
-
-    $('.comment-form').bPopup({
-      modalClose: false,
-      opacity: 0.6,
-      positionStyle: 'fixed',
-      modalColor: '#CFDBC5',
-      modalClose: true//'fixed' or 'absolute'
-    });
-
-    var tree = 0;
-
-    $(".comment-form .fa-tree").on('click', function(event){
-      tree = Number($(this).attr('class').split(" ").pop())
-      for(i=0; i < tree; i++){
-        $($(".comment-form .fa-tree")[i]).css("color","green")
-      }
-      for(i=tree; i< 5; i++){
-        $($(".comment-form .fa-tree")[i]).css("color","gray")
-      }
-    })
-
-
-    $('.comment-form button').on('click', function(event){
-
-      var rating = tree;
-      var difficulty = $(".comment-form input[type=range]").val();
-      var tip = $(".comment-form textarea[name*=tip").val();
-      var review = $(".comment-form textarea[name*=review]").val();
-      var date_hiked = $(".comment-form input[type=date]").val();
-
-      var user_request = $.ajax({
-        url:'/sessions',
-        type: "GET",
-        dataType: "json"
+      
+      $('.comment-form input[type=range]').change(function(){
+        range = $('.comment-form input[type=range]').val()
+        $(".comment-form #range").text(rangeHash[Math.floor(range)])
       })
+    } 
 
-      user_request.done(function(user) {
-        var pattern = new RegExp(/\d+/)
-        var trail_id = Number(pattern.exec(window.location.pathname))
-        var user_id = user.id
-
-        var request = $.ajax({
-          url: trail_id+"/comments",
-          type: "POST",
-          data: {comments: {user_id: user_id, trail_id: trail_id, rating: rating, difficulty: difficulty, tip: tip, review: review, date_hiked: date_hiked} },
-          dataType: "json"
-        });
-
-        request.done(function(response){
-          if (response.success === 0){
-            $(".comment-form").bPopup().close()
-            rating = 1;
-            $(".comment-form input[type=range]").val(3);
-            $(".comment-form textarea[name*=tip").val("");
-            $(".comment-form textarea[name*=review]").val("");
-            $(".comment-form input[type=date]").val("");
-          }
-          else if (response.success === 1){
-            var message = response.message;
-            $('.comment-form .message').show();
-            $('.comment-form .message').text(message);
-            $('.comment-form .message').css("color","red");
-          }
-        });
+    function showCommentForm() {
+      $('.comment-form').bPopup({
+        modalClose: false,
+        opacity: 0.6,
+        positionStyle: 'fixed',
+        modalColor: '#CFDBC5',
+        modalClose: true//'fixed' or 'absolute'
       });
-    })
+
+      $(".comment-form .fa-tree").on('click', function(event){
+        tree = Number($(this).attr('class').split(" ").pop())
+        for(i=0; i < tree; i++){
+          $($(".comment-form .fa-tree")[i]).css("color","green")
+        }
+        for(i=tree; i< 5; i++){
+          $($(".comment-form .fa-tree")[i]).css("color","gray")
+        }
+      })
+    };
+  });
+
+  $('.comment-form button').on('click', function(event){
+    var rating = tree;
+    console.log(tree)
+    var difficulty = Number($(".comment-form input[type=range]").val());
+    var tip = $(".comment-form textarea[name*=tip").val();
+    var review = $(".comment-form textarea[name*=review]").val();
+    var date_hiked = $(".comment-form input[type=date]").val();
+    var pattern = new RegExp(/\d+/)
+    var trail_id = Number(pattern.exec(window.location.pathname))
+    var request = $.ajax({
+      url: "/trails/"+trail_id+"/comments",
+      type: "POST",
+      data: {comments: {trail_id: trail_id, rating: rating, difficulty: difficulty, tip: tip, review: review, date_hiked: date_hiked} },
+      dataType: "json"
+    });  
+    request.done(function(response){
+      debugger;
+      if (response.success === 0){
+        $(".comment-form").bPopup().close()
+        $(".comment-form input[type=range]").val(range);
+        $(".comment-form textarea[name*=tip").val("");
+        $(".comment-form textarea[name*=review]").val("");
+        $(".comment-form input[type=date]").val("");
+      }
+      else if (response.success === 1){
+        var message = response.message;
+        $('.comment-form .message').show();
+        $('.comment-form .message').text(message);
+        $('.comment-form .message').css("color","red");
+      }
+    });
   });
 });
 
