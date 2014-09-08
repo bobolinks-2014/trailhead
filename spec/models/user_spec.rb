ba49c6b526ec34b9f1e995cf05d894fd7ee32d79
jrwalker <jrobinsonwalker@gmail.com>
2014-09-08 10:28:01 -0500

    user validations 100% covered in user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,18 @@ RSpec.describe User, :type => :model do
   	expect(user.email).to eq("cjryan28@gmail.com")
   end
 
+  it 'should allow only valid email addresses' do
+  	should allow_value('something@thing.com', 'valid_email@totallyvalid.com').for(:email)
+  end
+  
+
   it { should have_many(:comments) }
   it { should have_secure_password }
+  it { should validate_presence_of(:email)}
+  it { should validate_presence_of(:password)}
+  it { should ensure_length_of(:password).is_at_least(6)}
+  it { should validate_uniqueness_of(:email)}
+
+
 
 end
