f25ab2830aaa7d6bdaf4175a4c82799ae3f21454
Sebastian Radloff <sradloff23@gmail.com>
2014-09-09 12:40:20 -0500

    added new filed to Trails migration and seed for under_review boolean

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ CSV.foreach('db/trails.csv', headers: true) do |row|
 	description = row[6]
 	length = row[7]
 
-	Trail.create(name: name, latitude: latitude, longitude: longitude, length: length, city: city, state: state, description: description, length: length, url: url)
+	Trail.create(name: name, latitude: latitude, longitude: longitude, length: length, city: city, state: state, description: description, length: length, url: url, under_review: false)
 	puts "Seeding #{i}th trail" if i % 500 == 0
 	i += 1
 end
@@ -22,6 +22,9 @@ end
   User.create(username: Faker::Name.first_name , email: Faker::Internet.email, password: "1234567", password_confirmation: "1234567")
 end
 
-45.times do
-  Comment.create(user_id: rand(1..10), trail_id: 1, review: Faker::Lorem.paragraph(6), difficulty: rand(1..3), rating: rand(1..5), tip: Faker::Lorem.sentence(1), date_hiked: Time.now.ago(45) )
+i = 0
+5000.times do
+  Comment.create(user_id: rand(1..10), trail_id: rand(1..6000), review: Faker::Lorem.paragraph(6), difficulty: rand(1..3), rating: rand(1..5), tip: Faker::Lorem.sentence(1), date_hiked: Time.now.ago(45))
+  puts "Seeding #{i}th comment" if i % 1000 == 0
+  i += 1 
 end
