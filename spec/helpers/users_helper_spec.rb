06238c901cfd21a5dbf83be1b1aa39a3298dd75e
jrwalker <jrobinsonwalker@gmail.com>
2014-09-10 11:19:08 -0500

    commented out spec in the helper_spec files because we're not using it

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -9,7 +9,7 @@ require 'spec_helper'
 #       expect(helper.concat_strings("this","that")).to eq("this that")
 #     end
 #   end
+# # end
+# describe UsersHelper do
+#   pending "add some examples to (or delete) #{__FILE__}"
 # end
-describe UsersHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
