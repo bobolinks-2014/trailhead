3a0ee45282428bc625064f8e882888c1ba4be85a
Sebastian Radloff <sradloff23@gmail.com>
2014-09-09 21:49:12 -0500

    added cancel button to create trail in side bar

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -6,7 +6,14 @@ $(document).ready(function(){
 		$('.create-trail-button').hide();
 		$('.trail-form').show();
 	});
-	$('.trail-form').on('click', 'button', function(event){
+	$('.submit-create-trail').on('click', function(event){
+		event.preventDefault();
+		$('.home-top').slideDown();
+		$('.create-trail-button').show();
+		$('.trail-form').hide();
+	});
+
+	$('.cancel-create-trail').on('click', function(event){
 		event.preventDefault();
 		$('.home-top').slideDown();
 		$('.create-trail-button').show();
