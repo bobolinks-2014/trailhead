7f024dd0abe01aa3230fa4de6765983dd0a8a757
Ravi P <ravip0711@gmail.com>
2014-09-10 08:13:33 -0700

    added errors for signup page, will write testing later

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,19 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       respond_to do |format|
-        format.json {render json: {success: 0, message: "Thanks for signing up!"} }
+        format.json {render json: {success: 0, message: "Thanks For Signing Up!"} }
+      end
+    elsif User.find_by(:email => @user.email)
+      respond_to do |format|
+        format.json {render json: {success: 1, message: "Email Already In Database!"} }
+      end
+    elsif @user.password.blank? || @user.password.length < 6
+      respond_to do |format|
+        format.json {render json: {success: 1, message: "Password Must Be 6 or More Characters!"} }
       end
     else
       respond_to do |format|
-        format.json {render json: {success: 1, message: "Password must be 6 or more characters"} }
+        format.json {render json: {success: 1, message: "Password Does Not Match!"} }
       end
     end
   end
